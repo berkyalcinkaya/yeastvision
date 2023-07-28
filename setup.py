afcc3db7cee16a343c795c39f773d5f67f2992b2
Berk <berkyalcinkaya55@gmail.com>
2023-07-28 12:27:12 -0400

    setup.py modified

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ packages = ["yeastvision", "yeastvision.plot", "yeastvision.track", "yeastvision
 
 setup(
     name = "yeastvision",
-    version = "0.1.9",
+    version = "0.2.0",
     description = "Deep learning-enabled image analysis of the yeast full life cycle",
     author = "Berk Yalcinkaya",
     url = "https://github.com/berkyalcinkaya/yeastvision",
